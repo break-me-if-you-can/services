a5b4695e3e4399e830d58ac4f66681a7c0a725c0
mykyta-protsenko <mykyta@protsenko.com>
2018-08-26 15:58:52 -0700

    Added scores/submit, made top/k configurable (#5)

--- leaderboard/src/test/java/xyz/breakit/leaderboard/rest/LeaderboardControllerTest.java
+++ leaderboard/src/test/java/xyz/breakit/leaderboard/rest/LeaderboardControllerTest.java
@@ -3,10 +3,10 @@ package xyz.breakit.leaderboard.rest;
 import com.google.common.io.Resources;
 import org.junit.Test;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import xyz.breakit.leaderboard.service.ImmutableLeaderboardEntry;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-
 
 public class LeaderboardControllerTest {
 
@@ -14,10 +14,18 @@ public class LeaderboardControllerTest {
 
     @Test
     public void testTop10() throws Exception {
-        webTestClient.get().uri("/top10")
+        webTestClient.get().uri("/top/10")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody().json(top10());
+    }
+
+    @Test
+    public void testSubmit() throws Exception {
+        webTestClient.post().uri("/scores/submit")
+                .syncBody(ImmutableLeaderboardEntry.builder().name("name").score(100).build())
+                .exchange()
+                .expectStatus().isOk();
     }
 
     private String top10() throws IOException {
