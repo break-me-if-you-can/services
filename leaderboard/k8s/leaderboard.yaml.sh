d610a8b0397786fa014dd00810cb8f8ad87bd6cf
Mykyta Protsenko <mykyta@protsenko.com>
2018-08-26 13:21:21 -0700

    Empty leaderboard

---
+++ leaderboard/k8s/leaderboard.yaml.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+cat <<YAML
+apiVersion: apps/v1beta1
+kind: Deployment
+metadata:
+  name: leaderboard
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: leaderboard
+    spec:
+      containers:
+        - name: leaderboard
+          image: gcr.io/$GCP_PROJECT/leaderboard:latest
+          imagePullPolicy: Always
+          ports:
+            - containerPort: 8080
+          env:
+            - name: foobar
+              value: "$(date +%s)"
+            - name: ZIPKIN_SERVICE_HOST
+              value: "zipkin"
+            - name: ZIPKIN_SERVICE_PORT
+              value: "9411"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: leaderboard
+spec:
+  type: LoadBalancer
+  selector:
+    app: leaderboard
+  ports:
+   - port: 8080
+     targetPort: 8080
+     protocol: TCP
+     name: rest
+YAML
