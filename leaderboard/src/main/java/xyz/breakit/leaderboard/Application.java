1ea4513732b138a3fb8c4622fd2f5436ab93c85d
Mykyta Protsenko <mykyta@protsenko.com>
2018-09-01 14:32:15 -0700

    Integrated Leaderboard with zipkin

--- leaderboard/src/main/java/xyz/breakit/leaderboard/Application.java
+++ leaderboard/src/main/java/xyz/breakit/leaderboard/Application.java
@@ -2,9 +2,16 @@ package xyz.breakit.leaderboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.SpanAdjuster;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public SpanAdjuster customSpanAdjuster() {
+		return span -> span.toBuilder().name("#leaderboard/" + span.name().replace("http:/", "")).build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
