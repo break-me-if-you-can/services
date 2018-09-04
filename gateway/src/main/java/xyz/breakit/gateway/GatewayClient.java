cb8c8a827663329ab2cf503a4b73e4f66126da3d
Alex Borysov <alexb.btw@gmail.com>
2018-09-03 21:51:26 -0700

    Gateway gRPC API for leaderboard and its dummy implementation. (#12)
    * Gateway gRPC API for the leaderboard.
    * Empty leaderboard gRPC API implementation in Gateway.

--- gateway/src/main/java/xyz/breakit/gateway/GatewayClient.java
+++ gateway/src/main/java/xyz/breakit/gateway/GatewayClient.java
@@ -1,8 +1,9 @@
 package xyz.breakit.gateway;
 
-import io.grpc.ManagedChannel;
-import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
 import xyz.breakit.gateway.FixtureServiceGrpc.FixtureServiceBlockingStub;
+import xyz.breakit.gateway.LeaderboardServiceGrpc.LeaderboardServiceBlockingStub;
 
 /**
  * Client to call gateway service.
@@ -11,17 +12,29 @@ public class GatewayClient {
 
     public static void main(String[] args) {
         String host = "35.230.13.179";
-        ManagedChannel channel = NettyChannelBuilder.forAddress(host, 8080)
-                .usePlaintext().build();
+        Channel channel = ManagedChannelBuilder.forAddress(host, 8080).usePlaintext().build();
+        callFixtureService(channel);
+        callLeaderboardService(channel);
+    }
+
+    private static void callFixtureService(Channel channel) {
         FixtureServiceBlockingStub client = FixtureServiceGrpc.newBlockingStub(channel);
 
-        GetFixtureRequest request = GetFixtureRequest.newBuilder()
+        GetFixtureRequest fixtureRequest = GetFixtureRequest.newBuilder()
                 .setLinesCount(10)
                 .setLineWidth(10)
                 .build();
 
-        FixtureResponse response = client.getFixture(request);
-        System.out.println("Response: " + response);
+        FixtureResponse fixture = client.getFixture(fixtureRequest);
+        System.out.println("FixtureResponse: " + fixture);
+    }
+
+    private static void callLeaderboardService(Channel channel) {
+        LeaderboardServiceBlockingStub client = LeaderboardServiceGrpc.newBlockingStub(channel);
+
+        TopScoresRequest topScoresRequest = TopScoresRequest.newBuilder().setSize(3).build();
+        TopScoresResponse topScores = client.getTopScores(topScoresRequest);
+        System.out.println("TopScoresResponse: " + topScores);
     }
 
 }
