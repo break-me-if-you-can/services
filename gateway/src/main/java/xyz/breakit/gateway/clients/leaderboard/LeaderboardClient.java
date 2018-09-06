35369d43a99b7adf7d77832553e29274c12de7e5
Mykyta Protsenko <mykyta@protsenko.com>
2018-09-05 22:18:01 -0700

    Fixed Sleuth sampling Enabled retries. Commented out CircuitBreaker for a while

--- gateway/src/main/java/xyz/breakit/gateway/clients/leaderboard/LeaderboardClient.java
+++ gateway/src/main/java/xyz/breakit/gateway/clients/leaderboard/LeaderboardClient.java
@@ -2,18 +2,36 @@ package xyz.breakit.gateway.clients.leaderboard;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.jodah.failsafe.CircuitBreaker;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.RetryPolicy;
 import okhttp3.*;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class LeaderboardClient {
 
+    // one per core
+    private static final ScheduledThreadPoolExecutor EXECUTOR = new ScheduledThreadPoolExecutor(4);
+
+    private static final CircuitBreaker CIRCUIT_BREAKER = new CircuitBreaker()
+            .withFailureThreshold(3, 5);
+
+
+    private final static RetryPolicy RETRY_POLICY = new RetryPolicy()
+            .withBackoff(1, 30, TimeUnit.SECONDS, 2.0)
+            .withJitter(300, TimeUnit.MILLISECONDS)
+            .retryOn(Throwable.class)
+            .withMaxRetries(5);
+
     private final String leaderboardUrl;
     private final OkHttpClient httpClient;
     private final ObjectMapper objectMapper;
+
 
     public LeaderboardClient(String leaderboardUrl) {
         this.leaderboardUrl = leaderboardUrl;
@@ -33,6 +51,14 @@ public class LeaderboardClient {
                 .build();
 
 
+        return Failsafe
+                .with(RETRY_POLICY)
+                //.with(CIRCUIT_BREAKER)
+                .with(EXECUTOR)
+                .future(() -> enqueueHttpRequest(request));
+    }
+
+    private CompletableFuture<List<LeaderboardEntry>> enqueueHttpRequest(Request request) throws IOException {
         CompletableFuture<List<LeaderboardEntry>> result = new CompletableFuture<>();
 
         httpClient.newCall(request).enqueue(new Callback() {
@@ -51,7 +77,6 @@ public class LeaderboardClient {
                         ));
             }
         });
-
         return result;
     }
 
@@ -70,7 +95,7 @@ public class LeaderboardClient {
                              }
 
                              @Override
-                             public void onResponse(Call call, Response response)  {
+                             public void onResponse(Call call, Response response) {
                                  if (response.code() == 200) {
                                      throw new RuntimeException("Got error code while updating a score: " + response.toString());
                                  }
