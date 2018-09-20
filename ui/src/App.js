68f57958be2b73fa862b39921a746017ebdd4791
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-09-19 21:07:58 -0700

    initial commit

---
+++ ui/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Game } from './Game';
+
+export class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+}
+
+render() {
+    return (
+        <div>
+            <Game />
+        </div>
+    )
+  }
+}
