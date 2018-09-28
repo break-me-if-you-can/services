834619f1603c7273b1ee718124d90050fcda2d87
Mykyta Protsenko <mykyta@protsenko.com>
2018-09-27 20:57:31 -0700

    Removed interim span

--- gateway/src/main/java/xyz/breakit/gateway/LeaderboardService.java
+++ gateway/src/main/java/xyz/breakit/gateway/LeaderboardService.java
@@ -1,8 +1,6 @@
 package xyz.breakit.gateway;
 
-import brave.ScopedSpan;
 import brave.Span;
-import brave.Tracer;
 import brave.Tracing;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
@@ -14,7 +12,6 @@ import xyz.breakit.gateway.clients.leaderboard.ImmutableLeaderboardEntry;
 import xyz.breakit.gateway.clients.leaderboard.LeaderboardClient;
 import xyz.breakit.gateway.clients.leaderboard.LeaderboardEntry;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
@@ -40,39 +37,23 @@ public class LeaderboardService extends LeaderboardServiceImplBase {
     @Override
     public void getTopScores(TopScoresRequest request,
                              StreamObserver<TopScoresResponse> responseObserver) {
+        CompletableFuture<List<LeaderboardEntry>> top5 = leaderboardClient.top5();
+        top5.whenCompleteAsync((l, e) -> {
+            if (e != null) {
+                responseObserver.onError(e);
+            } else {
+                List<PlayerScore> playerScores = l.stream()
+                        .map(score -> PlayerScore.newBuilder().setPlayerId(score.name()).setScore(score.score()).build())
+                        .collect(Collectors.toList());
 
+                TopScoresResponse response = TopScoresResponse.newBuilder()
+                        .addAllTopScores(playerScores)
+                        .build();
 
-        Span span = tracing.tracer().currentSpan()
-                .name("leaderboardtop5")
-                .start();
-
-        try (Tracer.SpanInScope originalScope = tracing.tracer().withSpanInScope(span)) {
-            LOG.info("Outer TraceId = {}", span.context().traceIdString());
-            CompletableFuture<List<LeaderboardEntry>> top5 = leaderboardClient.top5();
-            top5.whenCompleteAsync((l, e) -> {
-                try (Tracer.SpanInScope innerScope = tracing.tracer().withSpanInScope(span)) {
-                    innerScope.close();
-                    if (e != null) {
-                        span.error(e);
-                        responseObserver.onError(e);
-                    } else {
-                        List<PlayerScore> playerScores = l.stream()
-                                .map(score -> PlayerScore.newBuilder().setPlayerId(score.name()).setScore(score.score()).build())
-                                .collect(Collectors.toList());
-
-                        TopScoresResponse response = TopScoresResponse.newBuilder()
-                                .addAllTopScores(playerScores)
-                                .build();
-
-                        responseObserver.onNext(response);
-                        responseObserver.onCompleted();
-                    }
-                } finally {
-                    span.finish();
-                }
-            });
-        }
-
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+            }
+        });
     }
 
     @Override
