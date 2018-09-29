236f3a9077b1f90cecb841602c8ac85ca86a1317
Mykyta Protsenko <mykyta@protsenko.com>
2018-09-29 14:10:38 -0700

    Enabled envoy

---
+++ envoy/k8s/envoy.yaml.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+cat <<YAML
+apiVersion: apps/v1beta1
+kind: Deployment
+metadata:
+  name: envoy
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: envoy
+    spec:
+      containers:
+        - name: envoy
+          image: gcr.io/$GCP_PROJECT/envoy:latest
+          imagePullPolicy: Always
+          ports:
+            - containerPort: 8080
+          env:
+            - name: foobar
+              value: "$(date +%s)"
+            - name: GATEWAY_SERVICE_HOST
+              value: "gateway"
+            - name: GATEWAY_SERVICE_PORT
+              value: "8080"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: envoy
+spec:
+  type: LoadBalancer
+  selector:
+    app: envoy
+  ports:
+   - port: 80
+     targetPort: 8080
+     protocol: TCP
+     name: http
+YAML
