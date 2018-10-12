6be9dde432c79573f7f5135babfbd1e8050aed5b
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-10-11 20:37:27 -0700

    Removed comments

--- ui/src/gameobjects/Cloud.js
+++ ui/src/gameobjects/Cloud.js
@@ -5,8 +5,8 @@ export class Cloud extends PIXI.Sprite {
     constructor(props) {
         super(props.texture);
         this.anchor.set(0.5);
-        this.x = props.x; // 200 + 350 * Math.random();
-        this.y = props.y; // -50;
+        this.x = props.x;
+        this.y = props.y;
     }
 
     addToStage(stage) {
