874628c920879db2ed471e2d1eda3fcd5b2a6a33
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-10-16 03:46:31 -0700

    Moved assets import to a separate file

---
+++ ui/src/Assets.js
@@ -0,0 +1,21 @@
+import aircraftStraightImg from '../assets/textures/aircraft_straight.png';
+import aircraftLeftTurnSpriteSheetImg from '../assets/spritesheets/aircraft_turn_left.png';
+import aircraftRightTurnSpriteSheetImg from '../assets/spritesheets/aircraft_turn_right.png';
+import explosionSpriteSheetImg from '../assets/spritesheets/explosion.png';
+import gooseSpriteSheetImg from '../assets/spritesheets/goose.png';
+import cloudImg from '../assets/textures/cloud.png';
+import waterImg from '../assets/textures/water.png';
+import banksImg from '../assets/textures/banks.png';
+import portraitGif from '../assets/portrait.gif';
+
+export const IMAGES = {
+    AIRCRAFT_STRAIGHT: aircraftStraightImg,
+    AIRCRAFT_LEFT_TURN_SPRITESHEET: aircraftLeftTurnSpriteSheetImg,
+    AIRCRAFT_RIGHT_TURN_SPRITESHEET: aircraftRightTurnSpriteSheetImg,
+    EXPLOSION_SPRITESHEET: explosionSpriteSheetImg,
+    GOOSE_SPRITESHEET: gooseSpriteSheetImg,
+    CLOUD: cloudImg,
+    WATER: waterImg,
+    BANKS: banksImg,
+    PORTRAIT: portraitGif
+}
