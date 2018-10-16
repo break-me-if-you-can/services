3a9aa7b6c93f27bf49785d52f167dddf6f1976fd
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-10-16 04:23:07 -0700

    Changed imports; added retry cancellation

--- ui/src/Service.js
+++ ui/src/Service.js
@@ -1,38 +1,46 @@
 const grpc = {};
-    grpc.web = require('grpc-web');
+grpc.web = require('grpc-web');
 
-const { PlayerScore, UpdateScoreRequest, TopScoresRequest, GeneratePlayerIdRequest, GetFixtureRequest } = require("../generated/gateway_pb");
-const { LeaderboardServiceClient, FixtureServiceClient, PlayerIdServiceClient } = require("../generated/gateway_grpc_web_pb");
-const { InjectFailureRequest, PartialDegradationRequest } = require("../generated/admin/admin_pb");
-const { AdminServiceClient } = require("../generated/admin/admin_grpc_web_pb");
+import * as Requests from '../generated/gateway_pb';
+import * as Clients from '../generated/gateway_grpc_web_pb';
+// { PlayerScore, UpdateScoreRequest, TopScoresRequest, GeneratePlayerIdRequest, GetFixtureRequest } = require("../generated/gateway_pb");
+//const { LeaderboardServiceClient, FixtureServiceClient, FixtureServicePromiseClient, PlayerIdServiceClient } = require("../generated/gateway_grpc_web_pb");
 
+const REQUEST_CANCEL_TIMEOUT = 900;
 export class Service {
   
     constructor(props) {
       //this.fixtureServiceClient = new FixtureServiceClient('http://' + GATEWAY_SERVICE_HOST);
-      this.fixtureServiceClient = new FixtureServiceClient('http://35.233.196.238');
-      this.playerIdServiceClient = new PlayerIdServiceClient('http://35.233.196.238');
-      this.leaderboardServiceClient = new LeaderboardServiceClient('http://35.233.196.238');
-      this.adminServiceClient = new AdminServiceClient('http://35.233.196.238');
+      this.fixtureServiceClient = new Clients.FixtureServiceClient('http://35.233.196.238');
+      this.playerIdServiceClient = new Clients.PlayerIdServiceClient('http://35.233.196.238');
+      this.leaderboardServiceClient = new Clients.LeaderboardServiceClient('http://35.233.196.238');
     }
   
     getFixture = (callback) => {
-        let getFixtureRequest = new GetFixtureRequest();
+        let getFixtureRequest = new Requests.GetFixtureRequest();
             getFixtureRequest.setLineWidth(767);
             getFixtureRequest.setLinesCount(1);
             getFixtureRequest.setGooseWidth(62);
             getFixtureRequest.setCloudWidth(62);
 
+        let requestCancelTimeout = setTimeout(function() { call.cancel() }, REQUEST_CANCEL_TIMEOUT);
         this.fixtureServiceClient
             .getFixture(getFixtureRequest, { },
                 function(err, response) {
-                    callback(response);
+                    clearTimeout(requestCancelTimeout);
+                
+                    if (err) {
+                        console.log(err.code);
+                        console.log(err.messge);
+                    } else {
+                        callback(response);
+                    }
                 }
             );
     }
 
     getPlayerId = (callback) => {
-        let generatePlayerIdRequest = new GeneratePlayerIdRequest();
+        let generatePlayerIdRequest = new Requests.GeneratePlayerIdRequest();
 
         this.playerIdServiceClient.generatePlayerId(generatePlayerIdRequest, { },
             function(err, response) {
@@ -42,7 +50,7 @@ export class Service {
     }
     
     getPlayerId = (callback) => {
-        let generatePlayerIdRequest = new GeneratePlayerIdRequest();
+        let generatePlayerIdRequest = new Requests.GeneratePlayerIdRequest();
 
         this.playerIdServiceClient.generatePlayerId(generatePlayerIdRequest, { },
             function(err, response) {
@@ -52,7 +60,7 @@ export class Service {
     }
 
     getTopPlayerScore = (callback) => {
-        let topScoresRequest = new TopScoresRequest();
+        let topScoresRequest = new Requests.TopScoresRequest();
         topScoresRequest.setSize(5);
 
         this.leaderboardServiceClient.getTopScores(topScoresRequest, { },
@@ -63,35 +71,14 @@ export class Service {
     }
     
     updatePlayerScore = (data, callback) => {
-        let playerScore = new PlayerScore();
+        let playerScore = new Requests.PlayerScore();
         playerScore.setPlayerId(data.playerId);
         playerScore.setScore(data.score);
         
-        let updateScoreRequest = new UpdateScoreRequest();
+        let updateScoreRequest = new Requests.UpdateScoreRequest();
         updateScoreRequest.setPlayerScore(playerScore);
 
         this.leaderboardServiceClient.updateScore(updateScoreRequest, { },
-            function(err, response) {
-                callback(response);
-            }
-        );
-    }
-
-    injectFailure = (callback) => {
-        let injectFailureRequest = new InjectFailureRequest();
-
-        this.adminServiceClient.injectFailure(injectFailureRequest, { }, 
-            function(err, reposnse) {
-                callback(response);
-            }
-        );
-    }
-
-    managePartialDegradation = (enable, callback) => {
-        let partialDegradationRequest = new PartialDegradationRequest();
-        partialDegradationRequest.setEnable(enable);
-
-        this.adminServiceClient.managePartialDegradation(partialDegradationRequest, { }, 
             function(err, response) {
                 callback(response);
             }
