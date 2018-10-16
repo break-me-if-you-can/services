2e05537ee3ea0eb43ae08ea28de174c2fc681a6f
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-10-16 03:52:08 -0700

    Added ParalaxTexture class for water and banks

---
+++ ui/src/gameobjects/ParallaxTexture.js
@@ -0,0 +1,22 @@
+import * as PIXI from 'pixi.js';
+
+export class ParallaxTexture extends PIXI.extras.TilingSprite {
+
+    constructor(props) {
+        super(props.image.texture, props.width, props.height);
+        this.anchor.set(0, 0);
+        this.position.x = props.horizontalOffset || 0;
+        this.position.y = props.verticalOffset || 0;
+        this.tilePosition.x = 0;
+        this.tilePosition.y = 0;
+    }
+
+    addToStage(stage) {
+        stage.addChild(this);
+    }
+
+    removeFromStage(stage) {
+        stage.removeChild(this);
+    }
+
+}
