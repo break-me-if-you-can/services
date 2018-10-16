421f35629300750b56c0921c43ead0769f311cf9
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-10-16 04:01:24 -0700

    Rearranged UI code (#79)
    * Moved assets import to a separate file
    
    * Moved constants to a separate file
    
    * Added ParalaxTexture class for water and banks
    
    * Updated aircraft class

--- ui/src/gameobjects/Aircraft.js
+++ ui/src/gameobjects/Aircraft.js
@@ -1,7 +1,21 @@
-import { Entity } from "./Entity";
+import * as PIXI from 'pixi.js';
+export class Aircraft extends PIXI.Sprite {
 
-export class Aircraft extends Entity{
     constructor(props) {
-
+        super(props.image.texture);
+        this.anchor.set(0.5);
+        this.width = props.width;
+        this.height = props.height;
+        this.x = props.x;
+        this.y = props.y;
     }
+
+    addToStage(stage) {
+        stage.addChild(this);
+    }
+
+    removeFromStage(stage) {
+        stage.removeChild(this);
+    }
+
 }
