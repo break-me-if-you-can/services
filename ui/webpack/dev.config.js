ba4c5ff8581ee2444c1afdeec78b7106e1c3b927
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-10-29 01:44:53 -0700

    updated webpack config files and run app scripts

--- ui/webpack/dev.config.js
+++ ui/webpack/dev.config.js
@@ -1,68 +1,36 @@
 var path = require('path');
 var webpack = require('webpack');
-
-module.exports = {
-  devtool: 'eval',
-  stats: {
-    errorDetails: true
-  },
+var config = require('./webpack.config');
+var publicPath = config.output.publicPath;
+module.exports = Object.assign({}, config, {
   entry: [
-    'webpack-hot-middleware/client',
-    './src/index'
+    'webpack-dev-server/client?http://0.0.0.0:3001',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '../src/index')
   ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  resolve: {
-    extensions: ["", ".js", ".jsx", ".css", ".json", ".otf", ".ttf", ".png", ".gif"]
-  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.DefinePlugin({
+      'THEME': {
+        'ISNIGHT': JSON.stringify(false),
+        'PORT': JSON.stringify('3001'),
+      }
+    })
   ],
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel'],
-        include: [path.join(__dirname, 'src'), path.join(__dirname, 'generated')]
-      },
-      {
-        test: /\.(png|svg|gif)(\?[a-z0-9=.]+)?$/,
-        loader: 'url-loader?limit=100000',
-        include: path.join(__dirname, 'assets')
-      },
-      {
-        test: /\.(otf|ttf)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-          name: "fonts/[name].[ext]"
-        },
-        include: path.join(__dirname, 'assets')
-      },
-      {
-          test: /\.css$/,
-          loaders: ['style-loader', 'css-loader'],
-          include: [path.join(__dirname, 'src')]
-      },
-      {
-        test: /\.js$/,
-        loaders: ['transform?brfs'],
-        include: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loaders: ['json']
-      }
-    ],
-    postLoaders: [
-      {
-        include: path.resolve(__dirname, 'node_modules/pixi.js'),
-        loader: 'transform?brfs'
-      }
+  sassLoader:{
+    outputStyle:'expanded',
+    sourceMap: true,
+    includePaths: [
+      path.join(__dirname, '../src/assets/scss/dev'),
+      path.join(__dirname, '../node_modules')
     ]
+  },
+  devServer: {
+    compress: true,
+    contentBase: path.join(__dirname, '../public'),
+    publicPath: publicPath,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    hot: true
   }
-};
+});
