176e9d3df7cf7b3c8b6de7d9da47517b9e623fef
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-11-02 14:00:15 -0700

    added mode indication for the app

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+// console.log(`Demo is in ${process.env.NODE_ENV} mode.`);
+
 ReactDOM.render(<App />, document.getElementById('root'));
