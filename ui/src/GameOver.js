d27feb72b17bdf59dffcd373e74ee40cdafc9e29
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-11-05 21:55:16 -0800

    moved game over screen to a separate file

---
+++ ui/src/GameOver.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export function GameOver(props) {
+    return (<div className="game_over">
+              <div className="game_over content">
+                <div className="wrapper">
+                  <div>
+                    <p>game over!</p>
+                  </div>
+                  <div>
+                    <div className="goose_gameover"></div>
+                    <div className="goose_gameover"></div>
+                    <div className="goose_gameover"></div>
+                    <div className="goose_gameover"></div>
+                  </div>
+                  <div className="play_again" onClick={ props.onClick }>
+                    <p><a href="#">play again</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>);
+}
