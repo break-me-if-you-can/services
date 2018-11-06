d66e3cdf3150395007f85c9a17a669604e4b0300
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-11-05 21:54:08 -0800

    updated webpack config

--- ui/webpack/dev.config.js
+++ ui/webpack/dev.config.js
@@ -4,11 +4,7 @@ var config = require('./webpack.config');
 var publicPath = config.output.publicPath;
 module.exports = Object.assign({}, config, {
   mode: 'development',
-  entry: [
-    'webpack-hot-middleware/client?http://localhost:3001',
-    //'webpack/hot/only-dev-server',
-    path.join(__dirname, '../src/index')
-  ],
+  entry: path.join(__dirname, '../src/index'),
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
