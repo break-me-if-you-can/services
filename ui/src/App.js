f42ce887cba2ebe14aa28b89239d025417f19327
Yevgen Golubenko <yevgen.golubenko@gmail.com>
2018-11-07 20:56:36 -0800

    fixed issue with starting app in portrait mode (#99)

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import { Game } from './Game';
+import { Portrait } from './Messages';
 import './App.css';
 
 export class App extends Component {
   
-  constructor(props) {
-    super(props);
-}
+    constructor(props) {
+        super(props);
 
-render() {
-    return (
-        <div className="app">
-            <Game />
-        </div>
-    )
-  }
+        let isPortrait = window.innerHeight > window.innerWidth;
+        if (isPortrait) {
+            window.addEventListener("orientationchange", this.onOrientationChangedHandler, false);
+        }
+        this.state = {
+            portrait: isPortrait,
+        }
+    }
+
+    onOrientationChangedHandler = (e) => {
+        
+        setTimeout(() => {
+            console.log('H/W:', window.innerHeight, window.innerWidth);
+            let isPortrait = window.innerHeight > window.innerWidth;
+            this.setState({
+                portrait: isPortrait,
+            });
+
+            window.removeEventListener('orientationchange', this.onOrientationChangedHandler, false);
+        }, 200);
+    }
+    
+    render() {
+        let view = (this.state.portrait? (<Portrait />): (<Game />));
+
+        return (
+            <div className="app">
+                { view }
+            </div>
+        )
+    }
 }
