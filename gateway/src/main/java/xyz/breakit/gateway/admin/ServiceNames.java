81b9f0333bf97a4dd22337199fc7e1e5a5ad66ce
Mykyta Protsenko <mykyta@protsenko.com>
2019-11-30 18:30:09 -0800

    Revert "Slow generate player (#120)"
    This reverts commit 03b03326c4619b711d70b5ee534e53376ebae664.

--- gateway/src/main/java/xyz/breakit/gateway/admin/ServiceNames.java
+++ gateway/src/main/java/xyz/breakit/gateway/admin/ServiceNames.java
@@ -3,7 +3,7 @@ package xyz.breakit.gateway.admin;
 /**
  * Contains service names used to reporting and failure injection.
  */
-public interface ServiceNames {
+interface ServiceNames {
 
     String GATEWAY_SERVICE = "gateway";
     String GEESE_SERVICE = "geese";
