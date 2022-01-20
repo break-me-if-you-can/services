629fac2aafc664ceeab41576ea357d622301346e
Mykyta Protsenko <mykyta@protsenko.com>
2022-01-19 20:49:49 -0800

    Updated UI deps

--- ui/k8s/ui.yaml.sh
+++ ui/k8s/ui.yaml.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cat <<YAML
-apiVersion: apps/v1beta1
+apiVersion: apps/v1
 kind: Deployment
 metadata:
   name: ui
 spec:
   replicas: 1
+  selector:
+    matchLabels:
+      app: ui
   template:
     metadata:
       labels:
@@ -13,7 +16,7 @@ spec:
     spec:
       containers:
         - name: ui
-          image: gcr.io/$GCP_PROJECT/ui:latest
+          image: eu.gcr.io/$GCP_PROJECT/ui:latest
           imagePullPolicy: Always
           ports:
             - containerPort: 8100
